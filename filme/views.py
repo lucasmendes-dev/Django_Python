cd83a97d84e2374c3bea14ae6ed050a111c9f5ce
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-11-23 19:58:19 -0300

    construindo templates

--- filme/views.py
+++ filme/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Filme
 
-
+#FBV - Function Based Views
 def homepage(request):
    return render(request, "homepage.html")
-  
+
+#CBV - Class Based Views
+#class HomePage():
+#  pass
+
+
+def homefilmes(request):
+   context = {}
+   lista_filmes = Filme.objects.all()
+   context['lista_filmes'] = lista_filmes
+   
+   return render(request, 'homefilmes.html', context)
+
