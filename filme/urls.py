1b1394506caf750f027aa84ff92ba118a5f86ee7
Lucas Mendes <lucas.a.mendes97@hotmail.com>
2022-11-23 21:38:55 -0300

    routes, views and DBs

--- filme/urls.py
+++ filme/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-from .views import homepage, homefilmes
+from .views import  HomeFilmes, HomePage, DetalhesFilme
+
+
+app_name = 'filme'    #namespace no arquivo urls do projeto(dir hashflix)
 
 urlpatterns = [    
-    path('', homepage),
-    path('filmes/', homefilmes)
+    path('', HomePage.as_view(), name='homepage'),
+    path('filmes/', HomeFilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', DetalhesFilme.as_view(), name='detalhesfilme'),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 from .models import Filme
+from django.views.generic import TemplateView, ListView, DetailView
 
-#FBV - Function Based Views
-def homepage(request):
-   return render(request, "homepage.html")
 
 #CBV - Class Based Views
-#class HomePage():
-#  pass
+
+class HomePage(TemplateView):
+   template_name = "homepage.html"
 
 
-def homefilmes(request):
-   context = {}
-   lista_filmes = Filme.objects.all()
-   context['lista_filmes'] = lista_filmes
+class HomeFilmes(ListView):
+   template_name = "homefilmes.html"
+   model = Filme   #passa uma lista com o nome: 'object_list' para o template
+   # object_list -> lista de itens do modelo
    
-   return render(request, 'homefilmes.html', context)
 
+class DetalhesFilme(DetailView):
+   template_name = "detalhesfilme.html"
+   model = Filme
+   # object -> 1 item do modelo
+
+
+
+#FBV - Function Based Views
+#***exemplo usando functions (FBV)***
+
+#def homefilmes(request):
+#   context = {}
+#   lista_filmes = Filme.objects.all()
+#   context['lista_filmes'] = lista_filmes
+#   
+#   return render(request, 'homefilmes.html', context)
